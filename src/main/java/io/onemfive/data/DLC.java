df0d1745a9f3f30e1eede34015d8b91469625a93
Brian Taylor <objectorange@protonmail.com>
2018-05-17 13:22:13 -0400

    Moved common data keys to this class to reduce code.

---
+++ src/main/java/io/onemfive/data/DLC.java
@@ -0,0 +1,15 @@
+package io.onemfive.data;
+
+/**
+ * Data Location Constants
+ *
+ * @author objectorange
+ */
+public final class DLC {
+    public static final String CONTENT = "CONTENT";
+    public static final String ENTITY = "ENTITY";
+    public static final String HASH = "HASH";
+    public static final String HASHES = "HASHES";
+    public static final String NONE = "NONE";
+    public static final String SNAPSHOT = "SNAPSHOT";
+}
