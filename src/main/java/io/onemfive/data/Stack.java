bae480d131ea23dea28f3d204bd9b6fd9b6f5234
Brian Taylor <objectorange@protonmail.com>
2018-05-17 16:45:29 -0400

    needs to be serializable

--- src/main/java/io/onemfive/data/Stack.java
+++ src/main/java/io/onemfive/data/Stack.java
@@ -1,11 +1,13 @@
 package io.onemfive.data;
 
+import java.io.Serializable;
+
 /**
  * TODO: Add Description
  *
  * @author objectorange
  */
-public interface Stack<T> {
+public interface Stack<T> extends Serializable {
     void push(T object);
     T pop();
     T peek();
