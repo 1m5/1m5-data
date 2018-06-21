ba676102e11965549d618b9d6aa50f5b471226a7
ObjectOrange <objectorange@protonmail.com>
2018-06-21 01:34:39 -0400

    Add encryption and signing keys to DID

--- src/main/java/io/onemfive/data/DID.java
+++ src/main/java/io/onemfive/data/DID.java
@@ -6,6 +6,13 @@ import org.dizitart.no2.objects.Index;
 import org.dizitart.no2.objects.Indices;
 
 import java.io.Serializable;
+import java.security.Key;
+import java.security.KeyPair;
+import java.security.Principal;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * TODO: Add Description
@@ -18,6 +25,7 @@ import java.io.Serializable;
 public class DID implements Persistable, Serializable {
 
     public enum Status {UNREGISTERED, ACTIVE, SUSPENDED}
+    public enum Provider{I2P}
 
     @Id
     private Long id;
@@ -26,6 +34,11 @@ public class DID implements Persistable, Serializable {
     private Status status = Status.UNREGISTERED;
     private boolean verified = false;
     private boolean authenticated = false;
+    private KeyPair masterKeys;
+    // Identities = Signing Keys: Long-Lived
+    private Map<Provider,Map<String,KeyPair>> providerIdentities = new HashMap<>();
+    // Encryption Keys: Short-Lived
+    private Map<Provider,Map<String,KeyPair>> providerEncryptionKeys = new HashMap<>();
 
     public Long getId() {
         return id;
@@ -73,5 +86,69 @@ public class DID implements Persistable, Serializable {
 
     public void setAuthenticated(boolean authenticated) {
         this.authenticated = authenticated;
+    }
+
+    public KeyPair getMasterKeys() {
+        return masterKeys;
+    }
+
+    public void setMasterKeys(KeyPair masterKeys) {
+        this.masterKeys = masterKeys;
+    }
+
+    public boolean supportsProvider(Provider provider, String alias) {
+        return getIdentity(provider, alias) != null && getEncryptionKeys(provider, alias) != null;
+    }
+
+    public boolean addIdentity(Provider provider, String alias, KeyPair keyPair) {
+        Map<String,KeyPair> identities = providerIdentities.get(provider);
+        if(identities == null) {
+            identities = new HashMap<>();
+            providerIdentities.put(provider, identities);
+        }
+        identities.put(alias,keyPair);
+        return true;
+    }
+
+    public KeyPair getIdentity(Provider provider, String alias) {
+        Map<String,KeyPair> identities = providerIdentities.get(provider);
+        if(identities == null)
+            return null;
+        else
+            return identities.get(alias);
+    }
+
+    public boolean removeIdentity(Provider provider, String alias) {
+        Map<String,KeyPair> identities = providerIdentities.get(provider);
+        if(identities != null) {
+            identities.remove(alias);
+        }
+        return true;
+    }
+
+    public boolean addEncryptionKeys(Provider provider, String alias, KeyPair keyPair) {
+        Map<String,KeyPair> encryptionKeys = providerEncryptionKeys.get(provider);
+        if(encryptionKeys == null) {
+            encryptionKeys = new HashMap<>();
+            providerEncryptionKeys.put(provider, encryptionKeys);
+        }
+        encryptionKeys.put(alias, keyPair);
+        return true;
+    }
+
+    public KeyPair getEncryptionKeys(Provider provider, String alias) {
+        Map<String,KeyPair> encryptionKeys = providerEncryptionKeys.get(provider);
+        if(encryptionKeys == null)
+            return null;
+        else
+            return encryptionKeys.get(alias);
+    }
+
+    public boolean removeEncryptionKeys(Provider provider, String alias) {
+        Map<String,KeyPair> encryptionKeys = providerEncryptionKeys.get(provider);
+        if(encryptionKeys != null) {
+            encryptionKeys.remove(alias);
+        }
+        return true;
     }
 }
