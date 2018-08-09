646c70d1a499baa95a615d8d4278310d22035975
objectorange <objectorange@protonmail.com>
2018-08-09 11:57:03 -0400

    can not add Inkrypt until it is open sourced.

--- src/main/java/io/onemfive/data/Peer.java
+++ src/main/java/io/onemfive/data/Peer.java
@@ -7,7 +7,6 @@ package io.onemfive.data;
  */
 public class Peer implements Addressable {
 
-    public static final String NETWORK_INKRYPT = "INKRYPT";
     public static final String NETWORK_1M5 = "1M5";
     public static final String NETWORK_1MM = "1MM";
     public static final String NETWORK_I2P = "I2P";
