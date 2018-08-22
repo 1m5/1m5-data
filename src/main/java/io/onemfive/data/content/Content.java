68f552c9db23524e9f956277f3e78a9b6159fdfb
objectorange <objectorange@protonmail.com>
2018-08-22 04:30:59 -0400

    should be abstract

--- src/main/java/io/onemfive/data/content/Content.java
+++ src/main/java/io/onemfive/data/content/Content.java
@@ -15,17 +15,14 @@ import java.util.Map;
  *
  * @author objectorange
  */
-public class Content implements Addressable, JSONSerializable, Serializable {
+public abstract class Content implements Addressable, JSONSerializable, Serializable {
 
-    // Required if not root
-    private Content parent;
     // Required
     private String type;
     private Integer version = 0;
     protected DID author;
     private byte[] body;
     private String bodyEncoding;
-    protected List<Content> fragments = new ArrayList<>();
     private Long createdAt;
     private String hash;
     private String hashAlgorithm;
@@ -36,14 +33,6 @@ public class Content implements Addressable, JSONSerializable, Serializable {
     @Override
     public String getAddress() {
         return hash;
-    }
-
-    public Content getParent() {
-        return parent;
-    }
-
-    public void setParent(Content parent) {
-        this.parent = parent;
     }
 
     public String getType() {
@@ -71,18 +60,7 @@ public class Content implements Addressable, JSONSerializable, Serializable {
     }
 
     public byte[] getBody() {
-        if(fragments.size() > 0) {
-            StringBuilder b = new StringBuilder();
-            if (body != null) {
-                b.append(new String(body));
-            }
-            for (Content f : fragments) {
-                b.append(new String(f.getBody()));
-            }
-            return b.toString().getBytes();
-        } else {
-            return body;
-        }
+        return body;
     }
 
     public void setBody(byte[] body) {
@@ -99,18 +77,8 @@ public class Content implements Addressable, JSONSerializable, Serializable {
         this.bodyEncoding = bodyEncoding;
     }
 
-    public void addFragment(Content content) {
-        fragments.add(content);
-        content.setParent(this);
-        hash = null; // will get rebuilt on next getHash()
-        incrementVersion();
-    }
-
     private void incrementVersion() {
         version++;
-        if(parent != null) {
-            parent.incrementVersion();
-        }
     }
 
     public Long getCreatedAt() {
@@ -182,24 +150,12 @@ public class Content implements Addressable, JSONSerializable, Serializable {
 
     public Map<String,Object> toMap() {
         Map<String,Object> m = new HashMap<>();
-        if(parent!=null) {
-            m.put("parentHash",parent.getHash());
-            m.put("parentHashAlgorithm",parent.getHashAlgorithm());
-        }
         if(type!=null) m.put("type",type);
         if(body != null) m.put("body", new String(body));
         if(bodyEncoding != null) m.put("bodyEncoding",bodyEncoding);
         if(createdAt != null) m.put("createdAt",String.valueOf(createdAt));
         if(hash != null) m.put("hash", hash);
         if(hashAlgorithm != null) m.put("hashAlgorithm",hashAlgorithm);
-        if(fragments != null && fragments.size() > 0) {
-            StringBuilder sb = new StringBuilder();
-            List<Map<String,Object>> fM = new ArrayList<>();
-            for(Content f : fragments) {
-                fM.add(f.toMap());
-            }
-            m.put("fragments",fM);
-        }
         if(author != null) {
             m.put("author.alias", author.getAlias());
             m.put("author.identityHash", new String(author.getIdentityHash()));
@@ -209,37 +165,11 @@ public class Content implements Addressable, JSONSerializable, Serializable {
     }
 
     public void fromMap(Map<String,Object> m) {
-        if(m.containsKey("parentHash")) {
-            Content cp = new Content();
-            cp.setHash((String)m.get("parentHash"));
-            cp.setHashAlgorithm((String)m.get("parentHashAlgorithm"));
-            setParent(cp);
-        }
         if(m.containsKey("body")) setBody(((String)m.get("body")).getBytes());
         if(m.containsKey("bodyEncoding")) setBodyEncoding((String)m.get("bodyEncoding"));
         if(m.containsKey("createdAt")) setCreatedAt(Long.parseLong((String)m.get("createdAt")));
         if(m.containsKey("hash")) setHash((String)m.get("hash"));
         if(m.containsKey("hashAlgorithm")) setHashAlgorithm((String)m.get("hashAlgorithm"));
-        if(m.containsKey("fragments")){
-            List<Map<String,Object>> fragments = (List<Map<String,Object>>)m.get("fragments");
-            for(Map<String,Object> m2 : fragments){
-                String type = (String)m2.get("type");
-                if(type!=null){
-                    try {
-                        Object obj = Class.forName(type).newInstance();
-                        Content c = (Content)obj;
-                        c.fromMap(m2);
-                        addFragment(c);
-                    } catch (InstantiationException e) {
-                        e.printStackTrace();
-                    } catch (IllegalAccessException e) {
-                        e.printStackTrace();
-                    } catch (ClassNotFoundException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }
         if(m.containsKey("author.alias")) {
             DID did = new DID();
             did.setAlias((String)m.get("author.alias"));
