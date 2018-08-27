8b17c56d1dee31f8f7e139d78552c367f81b7c05
objectorange <objectorange@protonmail.com>
2018-08-26 23:24:29 -0400

    add method

--- src/main/java/io/onemfive/data/social/Group.java
+++ src/main/java/io/onemfive/data/social/Group.java
@@ -4,13 +4,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Group extends Party {
+
     private List<Individual> individuals = new ArrayList<>();
+
+    public void add(Individual i) {
+        individuals.add(i);
+    }
 
     public List<Individual> getIndividuals() {
         return individuals;
     }
 
-    public void setIndividuals(List<Individual> individuals) {
-        this.individuals = individuals;
+    public void setIndividuals(List<Individual> i) {
+        individuals = i;
     }
 }
