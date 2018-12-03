f8ca90f7777dea15384c47147e0dc4b8176e0e85
objectorange <objectorange@protonmail.com>
2018-12-03 15:07:59 -0500

    unused constants

--- src/main/java/io/onemfive/data/util/DLC.java
+++ src/main/java/io/onemfive/data/util/DLC.java
@@ -14,10 +14,6 @@ public final class DLC {
 
     public static final String CONTENT = "CONTENT";
     public static final String ENTITY = "ENTITY";
-    public static final String HASH = "HASH";
-    public static final String HASHES = "HASHES";
-    public static final String NONE = "NONE";
-    public static final String SNAPSHOT = "SNAPSHOT";
     public static final String EXCEPTIONS = "EXCEPTIONS";
 
     public static boolean addRoute(Class service, String operation, Envelope envelope) {
