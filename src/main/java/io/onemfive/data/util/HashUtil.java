7a7ef19f86b4ba6cc123bb1c6284240d104b1683
objectorange <objectorange@protonmail.com>
2018-12-03 16:01:58 -0500

    add short hashes for speed

--- src/main/java/io/onemfive/data/util/HashUtil.java
+++ src/main/java/io/onemfive/data/util/HashUtil.java
@@ -1,20 +1,46 @@
 package io.onemfive.data.util;
 
+import io.onemfive.data.DID;
+
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.PBEKeySpec;
 import java.math.BigInteger;
+import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import java.util.Hashtable;
 
 public class HashUtil {
 
-    public static String generateHash(String contentToHash) {
+    public static String generateShortHash(byte[] contentToHash) throws NoSuchAlgorithmException {
+        return generateShortHash(getSalt(), contentToHash);
+    }
+
+    public static String generateShortHash(String contentToHash) throws NoSuchAlgorithmException {
+        return generateShortHash(getSalt(), contentToHash.getBytes());
+    }
+
+    private static String generateShortHash(byte[] salt, byte[] contentToHash) throws NoSuchAlgorithmException {
+        MessageDigest md = MessageDigest.getInstance("SHA-1");
+        md.update(salt);
+        byte[] bytes = md.digest(contentToHash);
+        String hashString = toHex(salt)+":"+toHex(bytes);
+        return Base64.encode(hashString);
+    }
+
+    public static Boolean verifyShortHash(byte[] contentToVerify, String hashToVerify) throws NoSuchAlgorithmException {
+        String hashString = Base64.decodeToString(hashToVerify);
+        String[] parts = hashString.split(":");
+        byte[] bytes = fromHex(parts[0]);
+        String contentHash = generateShortHash(bytes, contentToVerify);
+        return hashToVerify.equals(contentHash);
+    }
+
+    public static String generateHash(String contentToHash) throws NoSuchAlgorithmException {
         int iterations = 1000;
-        byte[] salt = new byte[16];
+        byte[] salt = getSalt();
         byte[] hash;
         try {
-            SecureRandom.getInstance("SHA1PRNG").nextBytes(salt);
-
             PBEKeySpec spec = new PBEKeySpec(contentToHash.toCharArray(), salt, iterations, 64 * 8);
             SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
             hash = skf.generateSecret(spec).getEncoded();
@@ -71,13 +97,28 @@ public class HashUtil {
         return bytes;
     }
 
-//    public static void main(String[] args) {
-//        DID did = new DID();
-//        did.setAlias("Alice");
-//        String hash = HashUtil.generateHash(did.getAlias());
-//        System.out.println("Alias Hash: "+hash);
-//        Boolean aliasVerified = HashUtil.verifyHash("Alice", hash);
-//        System.out.println("Alias Verified: "+aliasVerified);
-//    }
+    private static byte[] getSalt() throws NoSuchAlgorithmException {
+        SecureRandom sr = SecureRandom.getInstance("SHA1PRNG");
+        byte[] salt = new byte[16];
+        sr.nextBytes(salt);
+        return salt;
+    }
+
+    public static void main(String[] args) {
+        DID did = new DID();
+        did.setAlias("Alice");
+        try {
+            String hash = HashUtil.generateHash(did.getAlias());
+            System.out.println("Alias Full Hash: "+hash);
+            Boolean aliasVerified = HashUtil.verifyHash("Alice", hash);
+            System.out.println("Alias Full Hash Verified: "+aliasVerified);
+            String shortHash = HashUtil.generateShortHash(did.getAlias());
+            System.out.println("Alias Short Hash: "+shortHash);
+            Boolean shortHashVerified = HashUtil.verifyShortHash(did.getAlias().getBytes(), shortHash);
+            System.out.println("Alias Short Hash Verified: "+shortHashVerified);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
