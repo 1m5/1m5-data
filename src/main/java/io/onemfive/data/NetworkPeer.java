4c058c70adc1ccb55b7111e8e62ecd640bca2197
objectorange <objectorange@protonmail.com>
2019-03-15 14:10:19 -0400

    add network helpers and description

--- src/main/java/io/onemfive/data/NetworkPeer.java
+++ src/main/java/io/onemfive/data/NetworkPeer.java
@@ -6,18 +6,23 @@ import java.util.Map;
 /**
  * A node on the 1M5 peer-to-peer network.
  *
+ * Maintains addresses of the base networked peers including Tor, I2P, and IDN.
+ *
+ * In 1m5-core, a NetworkPeer is provided set with an IMS address/fingerprint
+ * with Tor, I2P, and IDN set if their respective sensors are activated.
+ *
  * @author objectorange
  */
 public class NetworkPeer implements Addressable, JSONSerializable, PIIClearable {
 
     public enum Network {
-        // Invisible Matrix Services (1M5)
+        // Invisible Matrix Services (1M5) - https://github.com/1m5/1m5-core
         IMS,
-        // Invisible Direct Mesh (1M5 internal project)
-        IDM,
-        // Invisible Internet Project - https://geti2p.net/
+        // Invisible Direct Network (1DN) - https://github.com/1m5/1m5-1dn
+        IDN,
+        // Invisible Internet Project (I2P) - https://geti2p.net/
         I2P,
-        // TOR - https://www.torproject.org/
+        // The Onion Router (TOR) - https://www.torproject.org/
         TOR,
         // Clearnet
         CLEAR
@@ -27,13 +32,11 @@ public class NetworkPeer implements Addressable, JSONSerializable, PIIClearable 
     protected DID did;
 
     public NetworkPeer() {
-        network = Network.CLEAR.name();
-        did = new DID();
+        this(Network.CLEAR.name(), null, null);
     }
 
     public NetworkPeer(String network) {
-        this.network = network;
-        did = new DID();
+        this(network, null, null);
     }
 
     public NetworkPeer(String network, String username, String passphrase) {
@@ -75,6 +78,118 @@ public class NetworkPeer implements Addressable, JSONSerializable, PIIClearable 
 
     public void setAddress(String address) {
         did.getPublicKey().setAddress(address);
+    }
+
+    public String getIMSAddress(){
+        if(Network.IMS.name().equals(network))
+            return getAddress();
+        else
+            return did.getPeer(Network.IMS.name()).getAddress();
+    }
+
+    public void setIMSAddress(String address) {
+        if(Network.IMS.name().equals(network))
+            setAddress(address);
+        else
+            did.getPeer(Network.IMS.name()).setAddress(address);
+    }
+
+    public String getIMSFingerprint() {
+        if(Network.IMS.name().equals(network))
+            return getFingerprint();
+        else
+            return did.getPeer(Network.IMS.name()).getFingerprint();
+    }
+
+    public void setIMSFingerprint(String fingerprint) {
+        if(Network.IMS.name().equals(network))
+            setFingerprint(fingerprint);
+        else
+            did.getPeer(Network.IMS.name()).setFingerprint(fingerprint);
+    }
+
+    public String getTorAddress(){
+        if(Network.TOR.name().equals(network))
+            return getAddress();
+        else
+            return did.getPeer(Network.TOR.name()).getAddress();
+    }
+
+    public void setTorAddress(String address) {
+        if(Network.TOR.name().equals(network))
+            setAddress(address);
+        else
+            did.getPeer(Network.TOR.name()).setAddress(address);
+    }
+
+    public String getTorFingerprint() {
+        if(Network.TOR.name().equals(network))
+            return getFingerprint();
+        else
+            return did.getPeer(Network.TOR.name()).getFingerprint();
+    }
+
+    public void setTorFingerprint(String fingerprint) {
+        if(Network.TOR.name().equals(network))
+            setFingerprint(fingerprint);
+        else
+            did.getPeer(Network.TOR.name()).setFingerprint(fingerprint);
+    }
+
+    public String getI2PAddress(){
+        if(Network.I2P.name().equals(network))
+            return getAddress();
+        else
+            return did.getPeer(Network.I2P.name()).getAddress();
+    }
+
+    public void setI2PAddress(String address) {
+        if(Network.I2P.name().equals(network))
+            setAddress(address);
+        else
+            did.getPeer(Network.I2P.name()).setAddress(address);
+    }
+
+    public String getI2PFingerprint() {
+        if(Network.I2P.name().equals(network))
+            return getFingerprint();
+        else
+            return did.getPeer(Network.I2P.name()).getFingerprint();
+    }
+
+    public void setI2PFingerprint(String fingerprint) {
+        if(Network.I2P.name().equals(network))
+            setFingerprint(fingerprint);
+        else
+            did.getPeer(Network.I2P.name()).setFingerprint(fingerprint);
+    }
+
+    public String getIDNAddress(){
+        if(Network.IDN.name().equals(network))
+            return getAddress();
+        else
+            return did.getPeer(Network.IDN.name()).getAddress();
+    }
+
+    public void setIDNAddress(String address) {
+        if(Network.IDN.name().equals(network))
+            setAddress(address);
+        else
+            did.getPeer(Network.IDN.name()).setAddress(address);
+    }
+
+    public String getIDNFingerprint() {
+        if(Network.IDN.name().equals(network))
+            return getFingerprint();
+        else
+            return did.getPeer(Network.IDN.name()).getFingerprint();
+    }
+
+    public void setIDNFingerprint(String fingerprint) {
+        if(Network.IDN.name().equals(network))
+            setFingerprint(fingerprint);
+        else
+            did.getPeer(Network.IDN.name()).setFingerprint(fingerprint);
     }
 
     @Override
