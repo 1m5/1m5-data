a0074125f44faea68fcb83a90b437e1069527bc5
objectorange <objectorange@protonmail.com>
2019-03-16 11:32:49 -0400

    additional attribute and constructor setup

--- src/main/java/io/onemfive/data/NetworkPeer.java
+++ src/main/java/io/onemfive/data/NetworkPeer.java
@@ -30,9 +30,10 @@ public class NetworkPeer implements Addressable, JSONSerializable, PIIClearable 
 
     protected String network;
     protected DID did;
+    protected Boolean isLocal = false;
 
     public NetworkPeer() {
-        this(Network.CLEAR.name(), null, null);
+        this(Network.IMS.name(), null, null);
     }
 
     public NetworkPeer(String network) {
@@ -59,6 +60,9 @@ public class NetworkPeer implements Addressable, JSONSerializable, PIIClearable 
     }
 
     public void setDid(DID did) {
+        // Ensure 1M5 DID has an I2P network Peer
+        if(Network.IMS.name().equals(network) && did.getPeer(Network.I2P.name())==null)
+            did.addPeer(new NetworkPeer(Network.I2P.name()));
         this.did = did;
     }
 
@@ -90,8 +94,7 @@ public class NetworkPeer implements Addressable, JSONSerializable, PIIClearable 
     public void setIMSAddress(String address) {
         if(Network.IMS.name().equals(network))
             setAddress(address);
-        else
-            did.getPeer(Network.IMS.name()).setAddress(address);
+        did.getPeer(Network.IMS.name()).setAddress(address);
     }
 
     public String getIMSFingerprint() {
@@ -164,6 +167,25 @@ public class NetworkPeer implements Addressable, JSONSerializable, PIIClearable 
             did.getPeer(Network.I2P.name()).setFingerprint(fingerprint);
     }
 
+    /**
+     * Checks the status of this peer instance and supplied peer instance
+     * @param peer the NetworkPeer to check against
+     * @return int
+     *      -2 if supplied NetworkPeer is not ready
+     *      -1 if this NetworkPeer is not ready
+     *       0 if not the same and ready
+     *       1 if they are the same and ready
+     */
+    public int getI2PStatus(NetworkPeer peer) {
+        int result;
+        if(peer.getI2PAddress() == null || peer.getI2PAddress().isEmpty()) result = -2;
+        else if(getI2PAddress() == null || getI2PAddress().isEmpty()) result =  -1;
+        else if(getI2PAddress().equals(peer.getI2PAddress())) result = 1;
+        else if(getAddress() != null && getAddress().equals(peer.getAddress())) result = 1;
+        else result = 0;
+        return result;
+    }
+
     public String getIDNAddress(){
         if(Network.IDN.name().equals(network))
             return getAddress();
@@ -190,6 +212,14 @@ public class NetworkPeer implements Addressable, JSONSerializable, PIIClearable 
             setFingerprint(fingerprint);
         else
             did.getPeer(Network.IDN.name()).setFingerprint(fingerprint);
+    }
+
+    public Boolean isLocal() {
+        return isLocal;
+    }
+
+    public void setLocal(Boolean local) {
+        isLocal = local;
     }
 
     @Override
@@ -220,5 +250,39 @@ public class NetworkPeer implements Addressable, JSONSerializable, PIIClearable 
         clone.did = (DID)did.clone();
         clone.network = network;
         return clone;
+    }
+
+    @Override
+    public String toString() {
+        StringBuffer sb = new StringBuffer();
+
+        sb.append("{\n\tlocal: "+isLocal());
+
+        if(getIMSAddress()!=null && getIMSAddress().length()>80)
+            sb.append("\n\t1m5Address: "+getIMSAddress().substring(0,80)+"...");
+        else
+            sb.append("\n\t1m5Address: "+getIMSAddress());
+        sb.append("\n\t1m5Fingerprint: "+getIMSFingerprint());
+
+        if(getTorAddress()!=null && getTorAddress().length()>80)
+            sb.append("\n\ttorAddress: "+getTorAddress().substring(0,80)+"...");
+        else
+            sb.append("\n\ttorAddress: "+getTorAddress());
+        sb.append("\n\ttorFingerprint: "+getTorFingerprint());
+
+        if(getI2PAddress()!=null && getI2PAddress().length()>80)
+            sb.append("\n\ti2pAddress: "+getI2PAddress().substring(0,80)+"...");
+        else
+            sb.append("\n\ti2pAddress: "+getI2PAddress());
+        sb.append("\n\ti2pFingerprint: "+getI2PFingerprint());
+
+        if(getIDNAddress()!=null && getIDNAddress().length()>80)
+            sb.append("\n\tidnAddress: "+getIDNAddress().substring(0,80)+"...");
+        else
+            sb.append("\n\tidnAddress: "+getIDNAddress());
+        sb.append("\n\tidnFingerprint: "+getIDNFingerprint());
+
+        sb.append("\n}");
+        return sb.toString();
     }
 }
