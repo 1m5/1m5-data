982f42224ce8cd86c5207afb77a7971a2001eab0
objectorange <objectorange@1m5.io>
2019-08-14 16:59:44 -0400

    support random integers

--- src/main/java/io/onemfive/data/util/RandomUtil.java
+++ src/main/java/io/onemfive/data/util/RandomUtil.java
@@ -11,4 +11,6 @@ public class RandomUtil {
     public static long nextRandomLong() {
         return new Random(System.currentTimeMillis()).nextLong();
     }
+    public static int nextRandomInteger() { return new Random(System.currentTimeMillis()).nextInt(); }
+    public static int nextRandomInteger(int upperBound) { return new Random(System.currentTimeMillis()).nextInt(upperBound); }
 }
